2078a8d06c0286d693073bf0bb05f660105d65f7
Joas-1999 <55988730+Joas-1999@users.noreply.github.com>
2022-05-08 15:27:01 +0200

    commit to 'main'

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// Mobile navigation
+const hamburger = document.querySelector('.hamburger');
+const menu = document.querySelector('.menu');
+const menuIcon = document.querySelector('i');
+
+hamburger.addEventListener('click', e => {
+    menu.classList.toggle('active');
+    menuIcon.classList.toggle('fa-xmark')
+})
+
